90a1ccf15544129d555ef7299791908eb14185b6
Lee Shi Jia <shijia@Lees-MacBook-Air.local>
2026-08-09 17:58:18 +0800

    Update CRUD test to use update_todo

--- test_api_crud.py
+++ test_api_crud.py
@@ -1,5 +1,4 @@
-from turtle import update
-from api_client.todos_client import create_todo, delete_todo, update_todo
+from api_client.todos_client import create_todo, delete_todo, get_todo_by_id, update_todo
 
 
 def test_update_todo():
@@ -25,6 +24,13 @@ def test_create_todo():
     assert response.status_code == 201
     assert response.json()["title"] == "Learn API automation"
 
+def test_read_todo():
+    response = get_todo_by_id(1)
+
+    assert response.status_code == 200 # check if the response status code is 200
+    assert response.json()["id"] == 1 # check if the response json id is 1
+    assert response.json()["title"] == "delectus aut autem" # check if the response json title is "Learn API automation"
+    assert response.json()["completed"] == False # check if the response json completed is False
 
 def test_delete_todo():
     response = delete_todo(1)
